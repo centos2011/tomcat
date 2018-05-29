2874b76822538f3416a6acd53e10bfc22492c5ce
Dennis Joson <dennis.joson@voyagerinnovation.com>
2018-05-29 10:22:04 +0800

    Added new files

---
+++ images/bin/image-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+exit_code=0;
+
+
+#cp ../../keys/jenkins.* ./keys &&\
+    docker build --no-cache -t tomcat-server .;
+if [[ "${?}" != 0 ]]; then
+    exit_code=1;
+fi;
+
+
+exit $((exit_code));
